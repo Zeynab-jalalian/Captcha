1ff941ad89ac904793b11782c3d5566d13c12633
zeynab jalalian <zjalalianweb@gmail.com>
2025-02-02 22:19:10 +0330

    generateCaptcha func

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ const input=document.querySelector('.input');
 const message=document.querySelector('.message');
 const refresh=document.querySelector('.refresh');
 const button=document.querySelector('.btn');
+
+
+let captchaText=null; //Captcha in captcha-field
+
+function generateCaptcha(){ //generate captcha
+    let randomC=Math.random().toString(36).substring(2,7);
+    let randomCArr=randomC.split("");
+    let changeRandomCArr=randomCArr.map(function(e){
+        if(Math.random() > 0.5){
+        return e.toUpperCase();
+        }else{
+        return e;
+        }
+    })
+    let randomCString=changeRandomCArr.join(" ");
+    
+}
+generateCaptcha();
