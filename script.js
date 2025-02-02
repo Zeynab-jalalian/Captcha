6a4b5c86906d590c0d1c8ac4e529a8d63527fa63
zeynab jalalian <zjalalianweb@gmail.com>
2025-02-02 22:08:25 +0330

    js dom

---
+++ script.js
@@ -0,0 +1,6 @@
+// DOM
+const captcha=document.querySelector('.captcha');
+const input=document.querySelector('.input');
+const message=document.querySelector('.message');
+const refresh=document.querySelector('.refresh');
+const button=document.querySelector('.btn');
