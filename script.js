c810622253d89c424778a799138ffb50b1b63a88
zeynab jalalian <zjalalianweb@gmail.com>
2025-02-02 11:40:15 +0330

    js dom

---
+++ script.js
@@ -0,0 +1,5 @@
+const captchaText=document.querySelector('.captch_box input');
+const captchInputBox=document.querySelector('.captch_input input');
+const refreshButton=document.querySelector('.refresh_button');
+const message=document.querySelector('.message');
+const submitButton=document.querySelector('.button');
