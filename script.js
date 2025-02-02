069d22cae466d78b3ac759d067258009cb61c1ec
zeynab jalalian <zjalalianweb@gmail.com>
2025-02-02 23:47:32 +0330

    js

--- script.js
+++ script.js
@@ -1,44 +1,41 @@
-// DOM
-const captcha=document.querySelector('.captcha input');
-const input=document.querySelector('.input input');
-const message=document.querySelector('.message');
-const refresh=document.querySelector('.refresh');
-const button=document.querySelector('.btn');
+const captcha = document.querySelector('.captcha input');
+const input = document.querySelector('.input input');
+const message = document.querySelector('.message');
+const refresh = document.querySelector('.refresh');
+const button = document.querySelector('.btn');
 
+let captchaText = null;
+function generateCaptcha() {
+    let randomC = Math.random().toString(36).substring(2, 7);
+    let randomArr = randomC.split("");
+    let changeArr = randomArr.map(function (e) {
+        if (Math.random() > 0.5) {
+            return e.toUpperCase();
 
-let captchaText=null; //Captcha in captcha-field
-
-function generateCaptcha(){ //generate captcha
-    let randomC=Math.random().toString(36).substring(2,7);
-    let randomCArr=randomC.split("");
-    let changeRandomCArr=randomCArr.map(function(e){
-        if(Math.random() > 0.5){
-        return e.toUpperCase();
-        }else{
-        return e;
+        } else {
+            return e;
         }
     })
-    captchaText=changeRandomCArr.join(" ");
-    captcha.value=captchaText;
-    
+    captchaText = changeArr.join(" ");
+    captcha.value = captchaText;
 }
 generateCaptcha();
-const refreshBtn=()=>{
+const ref = () => {
     generateCaptcha();
-    input.value="";
+    input.value = "";
     validate();
 }
-const validate=()=>{
- button.parentElement.classList.toggle("disabled",!input.value);
- if(input.value===""){
-   message.classList.remove("active");
- }
+const validate = () => {
+    button.parentElement.classList.toggle("disabled", !input.value);
+    if (input.value === "") {
+        message.classList.remove("active");
+    }
 }
-const submitBtn=()=>{
+const btn = () => {
     captchaText=captchaText
     .split("")
-    .filter((e)=> e !==" ")
-    .join("");
+    .filter((e)=>e!==" ")
+    .join("")
     message.classList.add("active");
     if(captchaText===input.value){
         message.innerHTML="Entered captcha is correct";
@@ -47,8 +44,7 @@ const submitBtn=()=>{
         message.innerHTML="Entered captcha is not correct"
         message.style.color="#ff2525"
     }
-
 }
-refresh.addEventListener("click",refreshBtn);
-input.addEventListener("keyup",validate);
-button.addEventListener("click",submitBtn)
+refresh.addEventListener("click", ref);
+input.addEventListener("keyup", validate);
+button.addEventListener("click", btn);
