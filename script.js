2f3fb9371952c47bc5282946ad8ae8551be85928
zeynab jalalian <zjalalianweb@gmail.com>
2025-02-02 12:04:44 +0330

    js ...

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-const captchaText=document.querySelector('.captch_box input');
+const captchaTextBox=document.querySelector('.captch_box input');
 const captchInputBox=document.querySelector('.captch_input input');
 const refreshButton=document.querySelector('.refresh_button');
 const message=document.querySelector('.message');
 const submitButton=document.querySelector('.button');
+
+let captchaText=null;
+
+const generateCaptcha= ()=>{
+    const randomString= Math.random().toString(36).substring(2,7);
+    console.log(randomString);
+    
+}
+generateCaptcha();
